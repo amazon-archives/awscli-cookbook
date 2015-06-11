5146b77743738ae4d2f701d2d9fc013305d57397
Peter Green <peter.green@aztek-native.com>
2015-06-11 11:26:31 +0100

    Adding guards to prevent Chef 11 issues with source_url and issues_url.

--- metadata.rb
+++ metadata.rb
@@ -5,8 +5,8 @@ license 'Apache 2.0'
 description 'Defines a number of LWRP wrapper commands around the awscli command line script'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '1.1.1'
-source_url 'https://github.com/awslabs/awscli-cookbook'
-issues_url 'https://github.com/awslabs/awscli-cookbook/issues'
+source_url 'https://github.com/awslabs/awscli-cookbook' if respond_to?(:source_url)
+issues_url 'https://github.com/awslabs/awscli-cookbook/issues' if respond_to?(:issues_url)
 
 supports 'ubuntu'
 supports 'centos'
